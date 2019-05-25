c7131763a155ceb15106a25d18e281b1a594032f
William Pescherine <wkpescherine@gmail.com>
2019-05-25 02:55:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,3 +21,11 @@ end
 def first_element_with_array_methods(array)
   array.first  
 end
+
+def last_element_with_array_methods(array)
+  array.last  
+end
+
+def length_of_array(array)
+  array.size
+end
