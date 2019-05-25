d5f638b33fa9bc61e095ce505c31cc0fb0522357
William Pescherine <wkpescherine@gmail.com>
2019-05-25 02:46:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,8 +6,8 @@ def array_with_two_elements
   array_with_two_elements = [1,2]
 end
 
-def first_element
-  first_element.first
+def first_element(array)
+  array.first
 end
 
 def third_element(array, element)
